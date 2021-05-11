3c7a0a7e7b10e1ae65aae9c1eea5ce4823fd1b71
ophub <chinaue@gmail.com>
2021-05-11 14:24:29 +0800

    update

---
+++ luci-app-amlogic/luasrc/model/cbi/amlogic/amlogic_info.lua
@@ -0,0 +1,15 @@
+local fs = require "luci.fs"
+local http = require "luci.http"
+local DISP = require "luci.dispatcher"
+local m
+
+--SimpleForm for Info
+m = Map("amlogic")
+m.title = translate("OpenWrt for Amlogic S9xxx STB")
+m.description = translate("Provide services such as install to EMMC / Backup and Recovery Config / Update Kernel for Amlogic STB.")
+m.pageaction = false
+
+m:section(SimpleSection).template  = "amlogic/other_info"
+
+
+return m
