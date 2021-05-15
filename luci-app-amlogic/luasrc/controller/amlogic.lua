9c8a270c8128135e59ec047ca4ef6ee00b454b87
ophub <chinaue@gmail.com>
2021-05-15 22:15:22 +0800

    update

--- luci-app-amlogic/luasrc/controller/amlogic.lua
+++ luci-app-amlogic/luasrc/controller/amlogic.lua
@@ -8,18 +8,27 @@ function index()
     entry({"admin", "system", "amlogic", "install"},cbi("amlogic/amlogic_install"),_("Install OpenWrt"), 2).leaf = true
     entry({"admin", "system", "amlogic", "backup"},cbi("amlogic/amlogic_backup"),_("Backup Config"), 3).leaf = true
     entry({"admin", "system", "amlogic", "upload"},cbi("amlogic/amlogic_upload"),_("Restore Config / Replace OpenWrt Kernel"), 4).leaf = true
+    entry({"admin", "system", "amlogic", "check"},cbi("amlogic/amlogic_check"),_("Config and Update"), 5).leaf = true
+    entry({"admin", "system", "amlogic", "check_plugin"},call("action_check_plugin"))
+    entry({"admin", "system", "amlogic", "check_kernel"},call("action_check_kernel"))
 	entry({"admin", "system", "amlogic", "refresh_log"},call("action_refresh_log"))
 	entry({"admin", "system", "amlogic", "del_log"},call("action_del_log"))
+	entry({"admin", "system", "amlogic", "start_check_plugin"},call("action_start_check_plugin")).leaf=true
+	entry({"admin", "system", "amlogic", "start_check_kernel"},call("action_start_check_kernel")).leaf=true
+	entry({"admin", "system", "amlogic", "state"},call("action_state")).leaf=true
 
 end
 
 local fs = require "luci.fs"
 
 function action_refresh_log()
-	local logfile="/tmp/amlogic.log"
+	local logfile="/tmp/amlogic/amlogic.log"
 	if not fs.access(logfile) then
 		--luci.http.write(os.date() .. ": No log")
-		luci.sys.exec("cat /proc/version > /tmp/amlogic.log && sync")
+		luci.sys.exec("mkdir -p /tmp/amlogic && sync")
+		luci.sys.exec("cat /proc/version > /tmp/amlogic/amlogic.log && sync")
+		luci.sys.exec("echo '' > /tmp/amlogic/amlogic_check_plugin.log && sync >/dev/null 2>&1")
+		luci.sys.exec("echo '' > /tmp/amlogic/amlogic_check_kernel.log && sync >/dev/null 2>&1")
 		--return
 	end
 	luci.http.prepare_content("text/plain; charset=utf-8")
@@ -31,6 +40,58 @@ function action_refresh_log()
 end
 
 function action_del_log()
-	luci.sys.exec(": > /tmp/amlogic.log")
+	luci.sys.exec(": > /tmp/amlogic/amlogic.log")
 	return
+end
+
+--后加的
+
+function action_check_plugin()
+	luci.sys.exec("chmod +x /usr/share/amlogic/amlogic_check_plugin.sh >/dev/null 2>&1")
+	return luci.sys.call("/usr/share/amlogic/amlogic_check_plugin.sh >/dev/null 2>&1")
+end
+
+function action_check_kernel()
+	luci.sys.exec("chmod +x /usr/share/amlogic/amlogic_check_kernel.sh >/dev/null 2>&1")
+	return luci.sys.call("/usr/share/amlogic/amlogic_check_kernel.sh >/dev/null 2>&1")
+end
+
+
+local function start_check_plugin()
+	return luci.sys.exec("sed -n '$p' /tmp/amlogic/amlogic_check_plugin.log 2>/dev/null")
+end
+
+local function start_check_kernel()
+	return luci.sys.exec("sed -n '$p' /tmp/amlogic/amlogic_check_kernel.log 2>/dev/null")
+end
+
+function action_start_check_plugin()
+	luci.http.prepare_content("application/json")
+	luci.http.write_json({
+			start_check_plugin = start_check_plugin();
+	})
+end
+
+function action_start_check_kernel()
+	luci.http.prepare_content("application/json")
+	luci.http.write_json({
+			start_check_kernel = start_check_kernel();
+	})
+end
+
+local function current_plugin_version()
+	return luci.sys.exec("uci get amlogic.config.amlogic_plugin_version 2>/dev/null") or "Invalid value."
+end
+
+local function current_kernel_version()
+	--return luci.sys.exec("uci get amlogic.config.amlogic_kernel_version 2>/dev/null") or "Invalid value."
+	return luci.sys.exec("ls /lib/modules/  2>/dev/null | grep -oE '^[1-9].[0-9]{1,2}.[0-9]+'")
+end
+
+function action_state()
+	luci.http.prepare_content("application/json")
+	luci.http.write_json({
+		current_plugin_version = current_plugin_version(),
+		current_kernel_version = current_kernel_version();
+	})
 end
--- luci-app-amlogic/luasrc/model/cbi/amlogic/amlogic_backup.lua
+++ luci-app-amlogic/luasrc/model/cbi/amlogic/amlogic_backup.lua
@@ -3,7 +3,7 @@ local http = require "luci.http"
 local DISP = require "luci.dispatcher"
 local m, b, mlog
 
---SimpleForm for Install OpenWrt to EMMC
+--SimpleForm for nil
 m = SimpleForm("", "", nil)
 m.reset = false
 m.submit = false
@@ -28,7 +28,7 @@ end
 o.write = function(self, section, scope)
 
 	local x = luci.sys.exec("chmod +x /usr/bin/openwrt-backup 2>/dev/null")
-	local r = luci.sys.exec("/usr/bin/openwrt-backup -b > /tmp/amlogic.log && sync 2>/dev/null")
+	local r = luci.sys.exec("/usr/bin/openwrt-backup -b > /tmp/amlogic/amlogic.log && sync 2>/dev/null")
 
 	local sPath, sFile, fd, block
 	sPath = "/.reserved/openwrt_config.tar.gz"
