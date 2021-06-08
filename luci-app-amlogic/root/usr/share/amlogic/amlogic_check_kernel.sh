a0040b5621a74d34cbab072605424bcbb42323c2
ophub <chinaue@gmail.com>
2021-06-08 13:26:12 +0800

    update

--- luci-app-amlogic/root/usr/share/amlogic/amlogic_check_kernel.sh
+++ luci-app-amlogic/root/usr/share/amlogic/amlogic_check_kernel.sh
@@ -8,7 +8,6 @@ START_LOG=${TMP_CHECK_DIR}"/amlogic_check_kernel.log"
 LOG_FILE=${TMP_CHECK_DIR}"/amlogic.log"
 TMP_CHECK_SERVER_FILE=${TMP_CHECK_DIR}"/amlogic_check_server_kernel_file.txt"
 LOGTIME=$(date "+%Y-%m-%d %H:%M:%S")
-[[ -d ${KERNEL_DOWNLOAD_PATH} ]] || mkdir -p ${KERNEL_DOWNLOAD_PATH}
 [[ -d ${TMP_CHECK_DIR} ]] || mkdir -p ${TMP_CHECK_DIR}
 
 # Log function
@@ -33,78 +32,62 @@ tolog() {
     [[ -s ${TMP_CHECK_SERVER_FILE} ]] || tolog "02.02 Invalid kernel detection file." "1"
 
     source ${TMP_CHECK_SERVER_FILE} 2>/dev/null
-    SERVER_KERNEL_URL=${amlogic_kernel_url}
+    SERVER_KERNEL_URL=${amlogic_kernel_github_repository}
+    #SERVER_KERNEL_URL="https://api.github.com/repos/ophub/amlogic-s9xxx-openwrt/contents/amlogic-s9xxx/amlogic-kernel"
     [[ ! -z "${SERVER_KERNEL_URL}" ]] || tolog "02.03 The custom kernel download address is invalid." "1"
 
     # 03. Version comparison
     tolog "03. Compare versions."
+    MAIN_LINE_M=$(echo "${CURRENT_KERNEL_V}" | cut -d '.' -f1)
     MAIN_LINE_V=$(echo "${CURRENT_KERNEL_V}" | cut -d '.' -f2)
-    if [[ "${MAIN_LINE_V}" -gt "0" ]]; then
-        case "${MAIN_LINE_V}" in
-            10) SERVER_KERNEL_VERSION=${amlogic_kernel_0510_version}
-                SERVER_KERNEL_BOOT=${amlogic_kernel_0510_boot}
-                SERVER_KERNEL_DTB=${amlogic_kernel_0510_dtb}
-                SERVER_KERNEL_MODULES=${amlogic_kernel_0510_modules}
-                ;;
-            11) SERVER_KERNEL_VERSION=${amlogic_kernel_0511_version}
-                SERVER_KERNEL_BOOT=${amlogic_kernel_0511_boot}
-                SERVER_KERNEL_DTB=${amlogic_kernel_0511_dtb}
-                SERVER_KERNEL_MODULES=${amlogic_kernel_0511_modules}
-                ;;
-            12) SERVER_KERNEL_VERSION=${amlogic_kernel_0512_version}
-                SERVER_KERNEL_BOOT=${amlogic_kernel_0512_boot}
-                SERVER_KERNEL_DTB=${amlogic_kernel_0512_dtb}
-                SERVER_KERNEL_MODULES=${amlogic_kernel_0512_modules}
-                ;;
-            13) SERVER_KERNEL_VERSION=${amlogic_kernel_0513_version}
-                SERVER_KERNEL_BOOT=${amlogic_kernel_0513_boot}
-                SERVER_KERNEL_DTB=${amlogic_kernel_0513_dtb}
-                SERVER_KERNEL_MODULES=${amlogic_kernel_0513_modules}
-                ;;
-             *) SERVER_KERNEL_VERSION=${amlogic_kernel_0504_version}
-                SERVER_KERNEL_BOOT=${amlogic_kernel_0504_boot}
-                SERVER_KERNEL_DTB=${amlogic_kernel_0504_dtb}
-                SERVER_KERNEL_MODULES=${amlogic_kernel_0504_modules}
-                ;;
-        esac
-        SERVER_KERNEL_VERSION_CODE=${SERVER_KERNEL_VERSION/.TF/}
-        tolog "03.01 current version: ${CURRENT_KERNEL_V}, Latest version: ${SERVER_KERNEL_VERSION_CODE}"
-        sleep 3
-    else
-        tolog "03.02 Failed to check kernel version." "1"
-    fi
+    MAIN_LINE_S=$(echo "${CURRENT_KERNEL_V}" | cut -d '.' -f3)
+    MAIN_LINE="${MAIN_LINE_M}.${MAIN_LINE_V}"
 
-    if [[ "${CURRENT_KERNEL_V}" == "${SERVER_KERNEL_VERSION_CODE}" ]]; then
-        tolog "03.03 The same version, no need to update." "1"
+    # Check the version on the server
+    LATEST_VERSION=$(curl -s "${SERVER_KERNEL_URL}" | grep "name" | grep -oE "${MAIN_LINE}.[0-9]+"  | sed -e "s/${MAIN_LINE}.//g" | sort -n | sed -n '$p')
+    #LATEST_VERSION="124"
+    [[ ! -z "${LATEST_VERSION}" ]] || tolog "03.01 Failed to get the version on the server." "1"
+    tolog "03.02 current version: ${CURRENT_KERNEL_V}, Latest version: ${MAIN_LINE}.${LATEST_VERSION}"
+    sleep 3
+
+    if [[ "${LATEST_VERSION}" -le "${MAIN_LINE_S}" ]]; then
+        tolog "03.02 Already the latest version, no need to upgrade." "1"
         sleep 5
         tolog ""
     else
-        tolog "03.04 Automatically download the latest kernel."
+        tolog "03.03 Automatically download the latest kernel."
+        sleep 3
 
         # Download boot file
-        wget -c "${SERVER_KERNEL_URL}/${SERVER_KERNEL_VERSION}/${SERVER_KERNEL_BOOT}" -O "${KERNEL_DOWNLOAD_PATH}/${SERVER_KERNEL_BOOT}" >/dev/null 2>&1 && sync
-        if [[ "$?" -eq "0" && -s "${KERNEL_DOWNLOAD_PATH}/${SERVER_KERNEL_BOOT}" ]]; then
-            tolog "03.05 ${SERVER_KERNEL_BOOT} complete."
+        SERVER_KERNEL_BOOT="$(curl -s "${SERVER_KERNEL_URL}/${MAIN_LINE}.${LATEST_VERSION}" | grep "download_url" | grep -o "https.*/boot-.*.tar.gz" | head -n 1)"
+        SERVER_KERNEL_BOOT_NAME="${SERVER_KERNEL_BOOT##*/}"
+        wget -c "${SERVER_KERNEL_BOOT}" -O "${KERNEL_DOWNLOAD_PATH}/${SERVER_KERNEL_BOOT_NAME}" >/dev/null 2>&1 && sync
+        if [[ "$?" -eq "0" ]]; then
+            tolog "03.04 The boot file complete."
         else
-            tolog "03.06 The boot file failed to download." "1"
+            tolog "03.05 The boot file failed to download." "1"
         fi
         sleep 3
 
         # Download dtb file
-        wget -c "${SERVER_KERNEL_URL}/${SERVER_KERNEL_VERSION}/${SERVER_KERNEL_DTB}" -O "${KERNEL_DOWNLOAD_PATH}/${SERVER_KERNEL_DTB}" >/dev/null 2>&1 && sync
-        if [[ "$?" -eq "0" && -s "${KERNEL_DOWNLOAD_PATH}/${SERVER_KERNEL_DTB}" ]]; then
-            tolog "03.07 ${SERVER_KERNEL_DTB} complete."
+        SERVER_KERNEL_DTB="$(curl -s "${SERVER_KERNEL_URL}/${MAIN_LINE}.${LATEST_VERSION}" | grep "download_url" | grep -o "https.*/dtb-amlogic-.*.tar.gz" | head -n 1)"
+        SERVER_KERNEL_DTB_NAME="${SERVER_KERNEL_DTB##*/}"
+        wget -c "${SERVER_KERNEL_DTB}" -O "${KERNEL_DOWNLOAD_PATH}/${SERVER_KERNEL_DTB_NAME}" >/dev/null 2>&1 && sync
+        if [[ "$?" -eq "0" ]]; then
+            tolog "03.06 The dtb file complete."
         else
-            tolog "03.08 The dtb file failed to download." "1"
+            tolog "03.07 The dtb file failed to download." "1"
         fi
         sleep 3
 
         # Download modules file
-        wget -c "${SERVER_KERNEL_URL}/${SERVER_KERNEL_VERSION}/${SERVER_KERNEL_MODULES}" -O "${KERNEL_DOWNLOAD_PATH}/${SERVER_KERNEL_MODULES}" >/dev/null 2>&1 && sync
-        if [[ "$?" -eq "0" && -s "${KERNEL_DOWNLOAD_PATH}/${SERVER_KERNEL_MODULES}" ]]; then
-            tolog "03.09 ${SERVER_KERNEL_MODULES} complete."
+        SERVER_KERNEL_MODULES="$(curl -s "${SERVER_KERNEL_URL}/${MAIN_LINE}.${LATEST_VERSION}" | grep "download_url" | grep -o "https.*/modules-.*.tar.gz" | head -n 1)"
+        SERVER_KERNEL_MODULES_NAME="${SERVER_KERNEL_MODULES##*/}"
+        wget -c "${SERVER_KERNEL_MODULES}" -O "${KERNEL_DOWNLOAD_PATH}/${SERVER_KERNEL_MODULES_NAME}" >/dev/null 2>&1 && sync
+        if [[ "$?" -eq "0" ]]; then
+            tolog "03.08 The modules file complete."
         else
-            tolog "03.10 The modules file failed to download." "1"
+            tolog "03.09 The modules file failed to download." "1"
         fi
         sleep 3
     fi
