d3513093cf2f1f946125f7431709e48ea6578470
ophub <ophub2020@gmail.com>
2021-07-22 21:22:05 +0800

    Optimized display

--- luci-app-amlogic/luasrc/model/cbi/amlogic/amlogic_info.lua
+++ luci-app-amlogic/luasrc/model/cbi/amlogic/amlogic_info.lua
@@ -5,7 +5,7 @@ local b
 
 --SimpleForm for Info
 b = SimpleForm("amlogic", translate("Amlogic Service"), nil)
-b.description = translate("Provide services such as install to EMMC, Update Firmware or Kernel, Backup and Recovery Config for Amlogic STB.")
+b.description = translate("Supports related operations on Amlogic series boxes through the panel.")
 b.reset = false
 b.submit = false
 b:section(SimpleSection).template  = "amlogic/other_info"
