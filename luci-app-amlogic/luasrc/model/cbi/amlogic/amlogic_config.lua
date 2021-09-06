cae12e7808422ac755a16cd9bac735db0a297172
ophub <ophub2020@gmail.com>
2021-09-06 14:44:24 +0800

    Support to switch the version branch of openwrt and kernel

--- luci-app-amlogic/luasrc/model/cbi/amlogic/amlogic_config.lua
+++ luci-app-amlogic/luasrc/model/cbi/amlogic/amlogic_config.lua
@@ -3,6 +3,12 @@ local http = require "luci.http"
 local DISP = require "luci.dispatcher"
 local b
 
+--Remove the spaces in the string
+function trim(str)
+   --return (string.gsub(str, "^%s*(.-)%s*$", "%1"))
+   return (string.gsub(str, "%s+", ""))
+end
+
 --Set Default value
 default_firmware_repo="ophub/amlogic-s9xxx-openwrt"
 local amlogic_firmware_repo = luci.sys.exec("uci get amlogic.config.amlogic_firmware_repo 2>/dev/null") or default_firmware_repo
@@ -13,11 +19,19 @@ local amlogic_firmware_tag = luci.sys.exec("uci get amlogic.config.amlogic_firmw
 default_firmware_suffix=".img.gz"
 local amlogic_firmware_suffix = luci.sys.exec("uci get amlogic.config.amlogic_firmware_suffix 2>/dev/null") or default_firmware_suffix
 
-default_firmware_config="1"
-local amlogic_firmware_config = luci.sys.exec("uci get amlogic.config.amlogic_firmware_config 2>/dev/null") or default_firmware_config
-
 default_kernel_path="amlogic-s9xxx/amlogic-kernel"
 local amlogic_kernel_path = luci.sys.exec("uci get amlogic.config.amlogic_kernel_path 2>/dev/null") or default_kernel_path
+
+local default_kernel_branch = luci.sys.exec("ls /lib/modules/ 2>/dev/null | grep -oE '^[1-9].[0-9]{1,3}'")
+local amlogic_kernel_branch = luci.sys.exec("uci get amlogic.config.amlogic_kernel_branch 2>/dev/null | grep -oE '^[1-9].[0-9]{1,3}'")
+if trim(amlogic_kernel_branch) == "" then
+    amlogic_kernel_branch = default_kernel_branch
+else
+    amlogic_kernel_branch = amlogic_kernel_branch
+end
+
+default_firmware_config="1"
+local amlogic_firmware_config = luci.sys.exec("uci get amlogic.config.amlogic_firmware_config 2>/dev/null") or default_firmware_config
 
 default_write_bootloader="1"
 local amlogic_write_bootloader = luci.sys.exec("uci get amlogic.config.amlogic_write_bootloader 2>/dev/null") or default_write_bootloader
@@ -47,7 +61,7 @@ o.write = function(self, key, value)
         --self.description = translate("Invalid value.")
         amlogic_firmware_repo = default_firmware_repo
     else
-        --self.description = translate("OpenWrt Firmware Repository:") .. value
+        --self.description = translate("Download repository of OpenWrt:") .. value
         amlogic_firmware_repo = value
     end
 end
@@ -62,7 +76,7 @@ o.write = function(self, key, value)
         --self.description = translate("Invalid value.")
         amlogic_firmware_tag = default_firmware_tag
     else
-        --self.description = translate("OpenWrt Releases Tag Keywords:") .. value
+        --self.description = translate("Keywords of Tags in Releases:") .. value
         amlogic_firmware_tag = value
     end
 end
@@ -77,7 +91,7 @@ o.write = function(self, key, value)
         --self.description = translate("Invalid value.")
         amlogic_firmware_suffix = default_firmware_suffix
     else
-        --self.description = translate("OpenWrt Firmware Suffix:") .. value
+        --self.description = translate("Suffix of OpenWrt files:") .. value
         amlogic_firmware_suffix = value
     end
 end
@@ -92,12 +106,31 @@ o.write = function(self, key, value)
         --self.description = translate("Invalid value.")
         amlogic_kernel_path = default_kernel_path
     else
-        --self.description = translate("OpenWrt Kernel DownLoad Path:") .. value
+        --self.description = translate("Download path of OpenWrt kernel:") .. value
         amlogic_kernel_path = value
     end
 end
 
---6.Restore configuration
+--6.Set kernel version branch
+o = s:option(Value, "kernel_branch", translate("Set version branch:"))
+o.description = translate("Set the version branch of the openwrt firmware and kernel selected in [Online Download Update].")
+o:value("5.4", translate("5.4"))
+o:value("5.10", translate("5.10"))
+o:value("5.13", translate("5.13"))
+o:value("5.14", translate("5.14"))
+o.default = tostring(amlogic_kernel_branch)
+o.rmempty = true
+o.write = function(self, key, value)
+    if value == "" then
+        --self.description = translate("Invalid value.")
+        amlogic_kernel_branch = "5.4"
+    else
+        --self.description = translate("Set version branch:") .. value
+        amlogic_kernel_branch = value
+    end
+end
+
+--7.Restore configuration
 o = s:option(Flag,"restore_config",translate("Keep config update:"))
 o.description = translate("Set whether to keep the current config during [Online Download Update] and [Manually Upload Update].")
 o.rmempty = false
@@ -114,7 +147,7 @@ o.write = function(self, key, value)
     end
 end
 
---7.Write bootloader
+--8.Write bootloader
 o = s:option(Flag,"auto_write_bootloader",translate("Auto write bootloader:"))
 o.description = translate("[Recommended choice] Set whether to auto write bootloader during install and update OpenWrt.")
 o.rmempty = false
@@ -131,7 +164,7 @@ o.write = function(self, key, value)
     end
 end
 
---8.Save button
+--9.Save button
 o = s:option(Button, "", translate("Save Config:"))
 o.template = "amlogic/other_button"
 o.render = function(self, section, scope)
@@ -145,8 +178,9 @@ o.write = function(self, section, scope)
     luci.sys.exec("uci set amlogic.config.amlogic_firmware_repo=" .. amlogic_firmware_repo .. " 2>/dev/null")
     luci.sys.exec("uci set amlogic.config.amlogic_firmware_tag=" .. amlogic_firmware_tag .. " 2>/dev/null")
     luci.sys.exec("uci set amlogic.config.amlogic_firmware_suffix=" .. amlogic_firmware_suffix .. " 2>/dev/null")
-    luci.sys.exec("uci set amlogic.config.amlogic_firmware_config=" .. amlogic_firmware_config .. " 2>/dev/null")
     luci.sys.exec("uci set amlogic.config.amlogic_kernel_path=" .. amlogic_kernel_path .. " 2>/dev/null")
+    luci.sys.exec("uci set amlogic.config.amlogic_kernel_branch=" .. amlogic_kernel_branch .. " 2>/dev/null")
+    luci.sys.exec("uci set amlogic.config.amlogic_firmware_config=" .. amlogic_firmware_config .. " 2>/dev/null")
     luci.sys.exec("uci set amlogic.config.amlogic_write_bootloader=" .. amlogic_write_bootloader .. " 2>/dev/null")
     luci.sys.exec("uci commit amlogic 2>/dev/null")
     http.redirect(DISP.build_url("admin", "system", "amlogic", "config"))
