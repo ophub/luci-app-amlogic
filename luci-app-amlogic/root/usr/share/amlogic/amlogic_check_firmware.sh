770eae914b395da9fa6f502c489d26eeccf20961
ophub <ophub2020@gmail.com>
2021-09-13 12:13:02 +0800

    Standard variable naming

--- luci-app-amlogic/root/usr/share/amlogic/amlogic_check_firmware.sh
+++ luci-app-amlogic/root/usr/share/amlogic/amlogic_check_firmware.sh
@@ -43,56 +43,56 @@ sleep 3
 # 01. Query local version information
 tolog "01. Query version information."
 # 01.01 Query the current version
-CURRENT_KERNEL_V=$(ls /lib/modules/  2>/dev/null | grep -oE '^[1-9].[0-9]{1,3}.[0-9]+')
-tolog "01.01 current version: ${CURRENT_KERNEL_V}"
+current_kernel_v=$(ls /lib/modules/  2>/dev/null | grep -oE '^[1-9].[0-9]{1,3}.[0-9]+')
+tolog "01.01 current version: ${current_kernel_v}"
 sleep 3
 
 # 01.01 Version comparison
-MAIN_LINE_VER=$(echo "${CURRENT_KERNEL_V}" | cut -d '.' -f1)
-MAIN_LINE_MAJ=$(echo "${CURRENT_KERNEL_V}" | cut -d '.' -f2)
-MAIN_LINE_VERSION="${MAIN_LINE_VER}.${MAIN_LINE_MAJ}"
+main_line_ver=$(echo "${current_kernel_v}" | cut -d '.' -f1)
+main_line_maj=$(echo "${current_kernel_v}" | cut -d '.' -f2)
+main_line_version="${main_line_ver}.${main_line_maj}"
 
 # 01.02. Query the selected branch in the settings
-SERVER_KERNEL_BRANCH=$(uci get amlogic.config.amlogic_kernel_branch 2>/dev/null | grep -oE '^[1-9].[0-9]{1,3}')
-if [[ -n "${SERVER_KERNEL_BRANCH}" && "${SERVER_KERNEL_BRANCH}" != "${MAIN_LINE_VERSION}" ]]; then
-    MAIN_LINE_VERSION="${SERVER_KERNEL_BRANCH}"
-    tolog "01.02 Select branch: ${MAIN_LINE_VERSION}"
+server_kernel_branch=$(uci get amlogic.config.amlogic_kernel_branch 2>/dev/null | grep -oE '^[1-9].[0-9]{1,3}')
+if [[ -n "${server_kernel_branch}" && "${server_kernel_branch}" != "${main_line_version}" ]]; then
+    main_line_version="${server_kernel_branch}"
+    tolog "01.02 Select branch: ${main_line_version}"
     sleep 3
 fi
 
 # 01.03. Download server version documentation
-SERVER_FIRMWARE_URL=$(uci get amlogic.config.amlogic_firmware_repo 2>/dev/null)
-[[ ! -z "${SERVER_FIRMWARE_URL}" ]] || tolog "01.03 The custom firmware download repo is invalid." "1"
-RELEASES_TAG_KEYWORDS=$(uci get amlogic.config.amlogic_firmware_tag 2>/dev/null)
-[[ ! -z "${RELEASES_TAG_KEYWORDS}" ]] || tolog "01.04 The custom firmware tag keywords is invalid." "1"
-FIRMWARE_SUFFIX=$(uci get amlogic.config.amlogic_firmware_suffix 2>/dev/null)
-[[ ! -z "${FIRMWARE_SUFFIX}" ]] || tolog "01.05 The custom firmware suffix is invalid." "1"
+server_firmware_url=$(uci get amlogic.config.amlogic_firmware_repo 2>/dev/null)
+[[ ! -z "${server_firmware_url}" ]] || tolog "01.03 The custom firmware download repo is invalid." "1"
+releases_tag_keywords=$(uci get amlogic.config.amlogic_firmware_tag 2>/dev/null)
+[[ ! -z "${releases_tag_keywords}" ]] || tolog "01.04 The custom firmware tag keywords is invalid." "1"
+firmware_suffix=$(uci get amlogic.config.amlogic_firmware_suffix 2>/dev/null)
+[[ ! -z "${firmware_suffix}" ]] || tolog "01.05 The custom firmware suffix is invalid." "1"
 
 # Supported format:
-# SERVER_FIRMWARE_URL="https://github.com/ophub/amlogic-s9xxx-openwrt"
-# SERVER_FIRMWARE_URL="ophub/amlogic-s9xxx-openwrt"
-if [[ ${SERVER_FIRMWARE_URL} == http* ]]; then
-    SERVER_FIRMWARE_URL=${SERVER_FIRMWARE_URL#*com\/}
+# server_firmware_url="https://github.com/ophub/amlogic-s9xxx-openwrt"
+# server_firmware_url="ophub/amlogic-s9xxx-openwrt"
+if [[ ${server_firmware_url} == http* ]]; then
+    server_firmware_url=${server_firmware_url#*com\/}
 fi
 
 # Delete other residual firmware files
-rm -f ${FIRMWARE_DOWNLOAD_PATH}/*${FIRMWARE_SUFFIX} 2>/dev/null && sync
+rm -f ${FIRMWARE_DOWNLOAD_PATH}/*${firmware_suffix} 2>/dev/null && sync
 rm -f ${FIRMWARE_DOWNLOAD_PATH}/*.img 2>/dev/null && sync
-rm -f /mnt/${EMMC_NAME}p4/*${FIRMWARE_SUFFIX} 2>/dev/null && sync
+rm -f /mnt/${EMMC_NAME}p4/*${firmware_suffix} 2>/dev/null && sync
 rm -f /mnt/${EMMC_NAME}p4/*.img 2>/dev/null && sync
 
-FIRMWARE_DOWNLOAD_URL="https:.*${RELEASES_TAG_KEYWORDS}.*${SOC}.*${MAIN_LINE_VERSION}.*${FIRMWARE_SUFFIX}"
+firmware_download_url="https:.*${releases_tag_keywords}.*${SOC}.*${main_line_version}.*${firmware_suffix}"
 
 # 02. Check Updated
 check_updated() {
     tolog "02. Start checking the updated ..."
 
     # Get the openwrt firmware updated_at
-    FIRMWARE_BROWSER_DOWNLOAD_LINE=$(curl -s "https://api.github.com/repos/${SERVER_FIRMWARE_URL}/releases" | grep -n "${FIRMWARE_DOWNLOAD_URL}" | awk -F ":" '{print $1}' | head -n 1)
-    if [[ -n "${FIRMWARE_BROWSER_DOWNLOAD_LINE}" && "${FIRMWARE_BROWSER_DOWNLOAD_LINE}" -gt "0" ]]; then
-        FIRMWARE_UPDATED_LINE=$(( FIRMWARE_BROWSER_DOWNLOAD_LINE - 1 ))
-        FIRMWARE_RELEASES_UPDATED=$(curl -s "https://api.github.com/repos/${SERVER_FIRMWARE_URL}/releases" | sed -n "${FIRMWARE_UPDATED_LINE}p" | cut -d '"' -f4 | cut -d 'T' -f1)
-        tolog '<input type="button" class="cbi-button cbi-button-reload" value="Download" onclick="return b_check_firmware(this, '"'download'"')"/> Latest updated: '${FIRMWARE_RELEASES_UPDATED}''
+    firmware_browser_download_line=$(curl -s "https://api.github.com/repos/${server_firmware_url}/releases" | grep -n "${firmware_download_url}" | awk -F ":" '{print $1}' | head -n 1)
+    if [[ -n "${firmware_browser_download_line}" && "${firmware_browser_download_line}" -gt "0" ]]; then
+        firmware_updated_line=$(( firmware_browser_download_line - 1 ))
+        firmware_releases_updated=$(curl -s "https://api.github.com/repos/${server_firmware_url}/releases" | sed -n "${firmware_updated_line}p" | cut -d '"' -f4 | cut -d 'T' -f1)
+        tolog '<input type="button" class="cbi-button cbi-button-reload" value="Download" onclick="return b_check_firmware(this, '"'download'"')"/> Latest updated: '${firmware_releases_updated}''
     else
         tolog "02.02 Invalid firmware check." "1"
     fi
@@ -104,18 +104,18 @@ check_updated() {
 download_firmware() {
     tolog "03. Download Openwrt firmware ..."
     # Get the openwrt firmware download path
-    FIRMWARE_RELEASES_PATH=$(curl -s "https://api.github.com/repos/${SERVER_FIRMWARE_URL}/releases" | grep "browser_download_url" | grep -o "${FIRMWARE_DOWNLOAD_URL}" | head -n 1)
-    FIRMWARE_DOWNLOAD_NAME="openwrt_${SOC}_k${MAIN_LINE_VERSION}_update${FIRMWARE_SUFFIX}"
-    wget -c "${FIRMWARE_RELEASES_PATH}" -O "${FIRMWARE_DOWNLOAD_PATH}/${FIRMWARE_DOWNLOAD_NAME}" >/dev/null 2>&1 && sync
-    if [[ "$?" -eq "0" && -s "${FIRMWARE_DOWNLOAD_PATH}/${FIRMWARE_DOWNLOAD_NAME}" ]]; then
+    firmware_releases_path=$(curl -s "https://api.github.com/repos/${server_firmware_url}/releases" | grep "browser_download_url" | grep -o "${firmware_download_url}" | head -n 1)
+    firmware_download_name="openwrt_${SOC}_k${main_line_version}_update${firmware_suffix}"
+    wget -c "${firmware_releases_path}" -O "${FIRMWARE_DOWNLOAD_PATH}/${firmware_download_name}" >/dev/null 2>&1 && sync
+    if [[ "$?" -eq "0" && -s "${FIRMWARE_DOWNLOAD_PATH}/${firmware_download_name}" ]]; then
         tolog "03.01 OpenWrt firmware download complete, you can update."
     else
         tolog "03.02 Invalid firmware download." "1"
     fi
     sleep 3
 
-    #echo '<a href="javascript:;" onclick="return amlogic_update(this, '"'${FIRMWARE_DOWNLOAD_NAME}'"')">Update</a>' >$START_LOG
-    tolog '<input type="button" class="cbi-button cbi-button-reload" value="Update" onclick="return amlogic_update(this, '"'${FIRMWARE_DOWNLOAD_NAME}'"')"/>'
+    #echo '<a href="javascript:;" onclick="return amlogic_update(this, '"'${firmware_download_name}'"')">Update</a>' >$START_LOG
+    tolog '<input type="button" class="cbi-button cbi-button-reload" value="Update" onclick="return amlogic_update(this, '"'${firmware_download_name}'"')"/>'
 
     exit 0
 }
--- luci-app-amlogic/root/usr/share/amlogic/amlogic_check_plugin.sh
+++ luci-app-amlogic/root/usr/share/amlogic/amlogic_check_plugin.sh
@@ -42,51 +42,51 @@ sleep 3
 
 # 01. Query local version information
 tolog "01. Query current version information."
-CURRENT_PLUGIN_V="$(opkg list-installed | grep 'luci-app-amlogic' | awk '{print $3}')"
-tolog "01.01 current version: ${CURRENT_PLUGIN_V}"
+current_plugin_v="$(opkg list-installed | grep 'luci-app-amlogic' | awk '{print $3}')"
+tolog "01.01 current version: ${current_plugin_v}"
 sleep 3
 
 # 02. Check the version on the server
 tolog "02. Query server version information."
-SERVER_PLUGIN_VERSION=$(curl -i -s "https://api.github.com/repos/ophub/luci-app-amlogic/releases" | grep "tag_name" | head -n 1 | grep -oE "[0-9]{1,3}.[0-9]{1,3}-[0-9]+")
-[ -n "${SERVER_PLUGIN_VERSION}" ] || tolog "02.01 Failed to get the version on the server." "1"
-tolog "02.02 current version: ${CURRENT_PLUGIN_V}, Latest version: ${SERVER_PLUGIN_VERSION}"
+server_plugin_version=$(curl -i -s "https://api.github.com/repos/ophub/luci-app-amlogic/releases" | grep "tag_name" | head -n 1 | grep -oE "[0-9]{1,3}.[0-9]{1,3}-[0-9]+")
+[ -n "${server_plugin_version}" ] || tolog "02.01 Failed to get the version on the server." "1"
+tolog "02.02 current version: ${current_plugin_v}, Latest version: ${server_plugin_version}"
 sleep 3
 
-if [[ "${CURRENT_PLUGIN_V}" == "${SERVER_PLUGIN_VERSION}" ]]; then
+if [[ "${current_plugin_v}" == "${server_plugin_version}" ]]; then
     tolog "02.03 Already the latest version, no need to update." "1"
     sleep 5
     tolog ""
 else
     tolog "02.04 Automatically download the latest plugin."
 
-    SERVER_PLUGIN_URL="https://github.com/ophub/luci-app-amlogic/releases/download"
-    SERVER_PLUGIN_FILE_IPK="luci-app-amlogic_${SERVER_PLUGIN_VERSION}_all.ipk"
-    SERVER_PLUGIN_FILE_I18N="luci-i18n-amlogic-zh-cn_${SERVER_PLUGIN_VERSION}_all.ipk"
-    SERVER_PLUGIN_FILE_LIBFS="luci-lib-fs_1.0-1_all.ipk"
+    server_plugin_url="https://github.com/ophub/luci-app-amlogic/releases/download"
+    server_plugin_file_ipk="luci-app-amlogic_${server_plugin_version}_all.ipk"
+    server_plugin_file_i18n="luci-i18n-amlogic-zh-cn_${server_plugin_version}_all.ipk"
+    server_plugin_file_libfs="luci-lib-fs_1.0-1_all.ipk"
 
     # Download plugin ipk file
-    wget -c "${SERVER_PLUGIN_URL}/${SERVER_PLUGIN_VERSION}/${SERVER_PLUGIN_FILE_IPK}" -O "${TMP_CHECK_DIR}/${SERVER_PLUGIN_FILE_IPK}" >/dev/null 2>&1 && sync
-    if [[ "$?" -eq "0" && -s "${TMP_CHECK_DIR}/${SERVER_PLUGIN_FILE_IPK}" ]]; then
-        tolog "02.05 ${SERVER_PLUGIN_FILE_IPK} complete."
+    wget -c "${server_plugin_url}/${server_plugin_version}/${server_plugin_file_ipk}" -O "${TMP_CHECK_DIR}/${server_plugin_file_ipk}" >/dev/null 2>&1 && sync
+    if [[ "$?" -eq "0" && -s "${TMP_CHECK_DIR}/${server_plugin_file_ipk}" ]]; then
+        tolog "02.05 ${server_plugin_file_ipk} complete."
     else
         tolog "02.06 The plugin file failed to download." "1"
     fi
     sleep 3
 
     # Download plugin i18n file
-    wget -c "${SERVER_PLUGIN_URL}/${SERVER_PLUGIN_VERSION}/${SERVER_PLUGIN_FILE_I18N}" -O "${TMP_CHECK_DIR}/${SERVER_PLUGIN_FILE_I18N}" >/dev/null 2>&1 && sync
-    if [[ "$?" -eq "0" && -s "${TMP_CHECK_DIR}/${SERVER_PLUGIN_FILE_I18N}" ]]; then
-        tolog "02.07 ${SERVER_PLUGIN_FILE_I18N} complete."
+    wget -c "${server_plugin_url}/${server_plugin_version}/${server_plugin_file_i18n}" -O "${TMP_CHECK_DIR}/${server_plugin_file_i18n}" >/dev/null 2>&1 && sync
+    if [[ "$?" -eq "0" && -s "${TMP_CHECK_DIR}/${server_plugin_file_i18n}" ]]; then
+        tolog "02.07 ${server_plugin_file_i18n} complete."
     else
         tolog "02.08 The plugin i18n failed to download." "1"
     fi
     sleep 3
 
     # Download plugin lib-fs file
-    wget -c "${SERVER_PLUGIN_URL}/${SERVER_PLUGIN_VERSION}/${SERVER_PLUGIN_FILE_LIBFS}" -O "${TMP_CHECK_DIR}/${SERVER_PLUGIN_FILE_LIBFS}" >/dev/null 2>&1 && sync
-    if [[ "$?" -eq "0" && -s "${TMP_CHECK_DIR}/${SERVER_PLUGIN_FILE_LIBFS}" ]]; then
-        tolog "02.09 ${SERVER_PLUGIN_FILE_LIBFS} complete."
+    wget -c "${server_plugin_url}/${server_plugin_version}/${server_plugin_file_libfs}" -O "${TMP_CHECK_DIR}/${server_plugin_file_libfs}" >/dev/null 2>&1 && sync
+    if [[ "$?" -eq "0" && -s "${TMP_CHECK_DIR}/${server_plugin_file_libfs}" ]]; then
+        tolog "02.09 ${server_plugin_file_libfs} complete."
     else
         tolog "02.10 The plugin i18n failed to download." "1"
     fi
