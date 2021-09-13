13e53f7dfa5c8546c39a47ab1d7bc14e8894e7d0
ophub <ophub2020@gmail.com>
2021-09-13 12:13:02 +0800

    Standard variable naming

--- luci-app-amlogic/root/usr/share/amlogic/amlogic_check_kernel.sh
+++ luci-app-amlogic/root/usr/share/amlogic/amlogic_check_kernel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set a fixed value
-CHECK_OPTION=${1}
-DOWNLOAD_VERSION=${2}
+check_option=${1}
+download_version=${2}
 EMMC_NAME=$(lsblk | grep -oE '(mmcblk[0-9])' | sort | uniq)
 KERNEL_DOWNLOAD_PATH="/mnt/${EMMC_NAME}p4/.tmp_upload"
 TMP_CHECK_DIR="/tmp/amlogic"
@@ -39,44 +39,44 @@ sleep 3
 #
 # 01. Download server version documentation
 tolog "01. Start checking the kernel version."
-SERVER_FIRMWARE_URL=$(uci get amlogic.config.amlogic_firmware_repo 2>/dev/null)
-[[ ! -z "${SERVER_FIRMWARE_URL}" ]] || tolog "01.01 The custom kernel download repo is invalid." "1"
-SERVER_KERNEL_PATH=$(uci get amlogic.config.amlogic_kernel_path 2>/dev/null)
-[[ ! -z "${SERVER_KERNEL_PATH}" ]] || tolog "01.02 The custom kernel download path is invalid." "1"
+server_firmware_url=$(uci get amlogic.config.amlogic_firmware_repo 2>/dev/null)
+[[ ! -z "${server_firmware_url}" ]] || tolog "01.01 The custom kernel download repo is invalid." "1"
+server_kernel_path=$(uci get amlogic.config.amlogic_kernel_path 2>/dev/null)
+[[ ! -z "${server_kernel_path}" ]] || tolog "01.02 The custom kernel download path is invalid." "1"
 #
 # Supported format:
 #
-# SERVER_FIRMWARE_URL="https://github.com/ophub/amlogic-s9xxx-openwrt"
-# SERVER_FIRMWARE_URL="ophub/amlogic-s9xxx-openwrt"
+# server_firmware_url="https://github.com/ophub/amlogic-s9xxx-openwrt"
+# server_firmware_url="ophub/amlogic-s9xxx-openwrt"
 #
-# SERVER_KERNEL_PATH="https://github.com/ophub/amlogic-s9xxx-openwrt/tree/main/amlogic-s9xxx/amlogic-kernel"
-# SERVER_KERNEL_PATH="https://github.com/ophub/amlogic-s9xxx-openwrt/trunk/amlogic-s9xxx/amlogic-kernel"
-# SERVER_KERNEL_PATH="amlogic-s9xxx/amlogic-kernel"
+# server_kernel_path="https://github.com/ophub/amlogic-s9xxx-openwrt/tree/main/amlogic-s9xxx/amlogic-kernel"
+# server_kernel_path="https://github.com/ophub/amlogic-s9xxx-openwrt/trunk/amlogic-s9xxx/amlogic-kernel"
+# server_kernel_path="amlogic-s9xxx/amlogic-kernel"
 #
-if [[ ${SERVER_FIRMWARE_URL} == http* ]]; then
-    SERVER_FIRMWARE_URL=${SERVER_FIRMWARE_URL#*com\/}
+if [[ ${server_firmware_url} == http* ]]; then
+    server_firmware_url=${server_firmware_url#*com\/}
 fi
 
-if [[ ${SERVER_KERNEL_PATH} == http* && $(echo ${SERVER_KERNEL_PATH} | grep "tree") != "" ]]; then
+if [[ ${server_kernel_path} == http* && $(echo ${server_kernel_path} | grep "tree") != "" ]]; then
     # Left part
-    SERVER_KERNEL_PATH_LEFT=${SERVER_KERNEL_PATH%\/tree*}
-    SERVER_KERNEL_PATH_LEFT=${SERVER_KERNEL_PATH_LEFT#*com\/}
-    SERVER_FIRMWARE_URL=${SERVER_KERNEL_PATH_LEFT}
+    server_kernel_path_left=${server_kernel_path%\/tree*}
+    server_kernel_path_left=${server_kernel_path_left#*com\/}
+    server_firmware_url=${server_kernel_path_left}
     # Right part
-    SERVER_KERNEL_PATH_RIGHT=${SERVER_KERNEL_PATH#*tree\/}
-    SERVER_KERNEL_PATH_RIGHT=${SERVER_KERNEL_PATH_RIGHT#*\/}
-    SERVER_KERNEL_PATH=${SERVER_KERNEL_PATH_RIGHT}
-elif [[ ${SERVER_KERNEL_PATH} == http* && $(echo ${SERVER_KERNEL_PATH} | grep "trunk") != "" ]]; then
+    server_kernel_path_right=${server_kernel_path#*tree\/}
+    server_kernel_path_right=${server_kernel_path_right#*\/}
+    server_kernel_path=${server_kernel_path_right}
+elif [[ ${server_kernel_path} == http* && $(echo ${server_kernel_path} | grep "trunk") != "" ]]; then
     # Left part
-    SERVER_KERNEL_PATH_LEFT=${SERVER_KERNEL_PATH%\/trunk*}
-    SERVER_KERNEL_PATH_LEFT=${SERVER_KERNEL_PATH_LEFT#*com\/}
-    SERVER_FIRMWARE_URL=${SERVER_KERNEL_PATH_LEFT}
+    server_kernel_path_left=${server_kernel_path%\/trunk*}
+    server_kernel_path_left=${server_kernel_path_left#*com\/}
+    server_firmware_url=${server_kernel_path_left}
     # Right part
-    SERVER_KERNEL_PATH_RIGHT=${SERVER_KERNEL_PATH#*trunk\/}
-    SERVER_KERNEL_PATH=${SERVER_KERNEL_PATH_RIGHT}
+    server_kernel_path_right=${server_kernel_path#*trunk\/}
+    server_kernel_path=${server_kernel_path_right}
 fi
 
-SERVER_KERNEL_URL="https://api.github.com/repos/${SERVER_FIRMWARE_URL}/contents/${SERVER_KERNEL_PATH}"
+server_kernel_url="https://api.github.com/repos/${server_firmware_url}/contents/${server_kernel_path}"
 # Step 1: URL formatting end -----------------------------------------------------------
 
 # Step 2: Check if there is the latest kernel version
@@ -84,39 +84,39 @@ check_kernel() {
     # 02. Query local version information
     tolog "02. Start checking the kernel version."
     # 02.01 Query the current version
-    CURRENT_KERNEL_V=$(ls /lib/modules/  2>/dev/null | grep -oE '^[1-9].[0-9]{1,2}.[0-9]+')
-    tolog "02.01 current version: ${CURRENT_KERNEL_V}"
+    current_kernel_v=$(ls /lib/modules/  2>/dev/null | grep -oE '^[1-9].[0-9]{1,2}.[0-9]+')
+    tolog "02.01 current version: ${current_kernel_v}"
     sleep 3
 
     # 02.02 Version comparison
-    MAIN_LINE_VER=$(echo "${CURRENT_KERNEL_V}" | cut -d '.' -f1)
-    MAIN_LINE_MAJ=$(echo "${CURRENT_KERNEL_V}" | cut -d '.' -f2)
-    MAIN_LINE_NOW=$(echo "${CURRENT_KERNEL_V}" | cut -d '.' -f3)
-    MAIN_LINE_VERSION="${MAIN_LINE_VER}.${MAIN_LINE_MAJ}"
+    main_line_ver=$(echo "${current_kernel_v}" | cut -d '.' -f1)
+    main_line_maj=$(echo "${current_kernel_v}" | cut -d '.' -f2)
+    main_line_now=$(echo "${current_kernel_v}" | cut -d '.' -f3)
+    main_line_version="${main_line_ver}.${main_line_maj}"
 
     # 02.03 Query the selected branch in the settings
-    SERVER_KERNEL_BRANCH=$(uci get amlogic.config.amlogic_kernel_branch 2>/dev/null | grep -oE '^[1-9].[0-9]{1,3}')
-    if [[ -n "${SERVER_KERNEL_BRANCH}" && "${SERVER_KERNEL_BRANCH}" != "${MAIN_LINE_VERSION}" ]]; then
-        MAIN_LINE_VERSION="${SERVER_KERNEL_BRANCH}"
-        MAIN_LINE_NOW="0"
-        tolog "02.02 Select branch: ${MAIN_LINE_VERSION}"
+    server_kernel_branch=$(uci get amlogic.config.amlogic_kernel_branch 2>/dev/null | grep -oE '^[1-9].[0-9]{1,3}')
+    if [[ -n "${server_kernel_branch}" && "${server_kernel_branch}" != "${main_line_version}" ]]; then
+        main_line_version="${server_kernel_branch}"
+        main_line_now="0"
+        tolog "02.02 Select branch: ${main_line_version}"
         sleep 3
     fi
 
     # Check the version on the server
-    LATEST_VERSION=$(curl -s "${SERVER_KERNEL_URL}" | grep "name" | grep -oE "${MAIN_LINE_VERSION}.[0-9]+"  | sed -e "s/${MAIN_LINE_VERSION}.//g" | sort -n | sed -n '$p')
-    #LATEST_VERSION="124"
-    [[ ! -z "${LATEST_VERSION}" ]] || tolog "02.03 Failed to get the version on the server." "1"
-    tolog "02.03 current version: ${CURRENT_KERNEL_V}, Latest version: ${MAIN_LINE_VERSION}.${LATEST_VERSION}"
+    latest_version=$(curl -s "${server_kernel_url}" | grep "name" | grep -oE "${main_line_version}.[0-9]+"  | sed -e "s/${main_line_version}.//g" | sort -n | sed -n '$p')
+    #latest_version="124"
+    [[ ! -z "${latest_version}" ]] || tolog "02.03 Failed to get the version on the server." "1"
+    tolog "02.03 current version: ${current_kernel_v}, Latest version: ${main_line_version}.${latest_version}"
     sleep 3
 
-    if [[ "${LATEST_VERSION}" -eq "${MAIN_LINE_NOW}" ]]; then
+    if [[ "${latest_version}" -eq "${main_line_now}" ]]; then
         tolog "02.04 Already the latest version, no need to update." "1"
         sleep 5
         tolog ""
         exit 0
     else
-        tolog '<input type="button" class="cbi-button cbi-button-reload" value="Download" onclick="return b_check_kernel(this, '"'download_${MAIN_LINE_VERSION}.${LATEST_VERSION}'"')"/> Latest version: '${MAIN_LINE_VERSION}.${LATEST_VERSION}''
+        tolog '<input type="button" class="cbi-button cbi-button-reload" value="Download" onclick="return b_check_kernel(this, '"'download_${main_line_version}.${latest_version}'"')"/> Latest version: '${main_line_version}.${latest_version}''
         exit 0
     fi
 }
@@ -124,9 +124,9 @@ check_kernel() {
 # Step 3: Download the latest kernel version
 download_kernel() {
     tolog "03. Start download the kernels."
-    if [[ ${DOWNLOAD_VERSION} == download* ]]; then
-        DOWNLOAD_VERSION=$(echo "${DOWNLOAD_VERSION}" | cut -d '_' -f2)
-        tolog "03.01 The kernel version: ${DOWNLOAD_VERSION}, downloading..."
+    if [[ ${download_version} == download* ]]; then
+        download_version=$(echo "${download_version}" | cut -d '_' -f2)
+        tolog "03.01 The kernel version: ${download_version}, downloading..."
     else
         tolog "03.02 Invalid parameter" "1"
     fi
@@ -139,48 +139,48 @@ download_kernel() {
     rm -f /mnt/${EMMC_NAME}p4/dtb-*.tar.gz 2>/dev/null && sync
     rm -f /mnt/${EMMC_NAME}p4/modules-*.tar.gz 2>/dev/null && sync
 
-    # Download boot file from the kernel directory under the path: ${SERVER_KERNEL_URL}/${DOWNLOAD_VERSION}/
-    SERVER_KERNEL_BOOT="$(curl -s "${SERVER_KERNEL_URL}/${DOWNLOAD_VERSION}" | grep "download_url" | grep -o "https.*/boot-${DOWNLOAD_VERSION}.*.tar.gz" | head -n 1)"
-    # Download boot file from current path: ${SERVER_KERNEL_URL}/
-    if [ -z "${SERVER_KERNEL_BOOT}" ]; then
-        SERVER_KERNEL_BOOT="$(curl -s "${SERVER_KERNEL_URL}" | grep "download_url" | grep -o "https.*/boot-${DOWNLOAD_VERSION}.*.tar.gz" | head -n 1)"
+    # Download boot file from the kernel directory under the path: ${server_kernel_url}/${download_version}/
+    server_kernel_boot="$(curl -s "${server_kernel_url}/${download_version}" | grep "download_url" | grep -o "https.*/boot-${download_version}.*.tar.gz" | head -n 1)"
+    # Download boot file from current path: ${server_kernel_url}/
+    if [ -z "${server_kernel_boot}" ]; then
+        server_kernel_boot="$(curl -s "${server_kernel_url}" | grep "download_url" | grep -o "https.*/boot-${download_version}.*.tar.gz" | head -n 1)"
     fi
-    SERVER_KERNEL_BOOT_NAME="${SERVER_KERNEL_BOOT##*/}"
-    SERVER_KERNEL_BOOT_NAME="${SERVER_KERNEL_BOOT_NAME//%2B/+}"
-    wget -c "${SERVER_KERNEL_BOOT}" -O "${KERNEL_DOWNLOAD_PATH}/${SERVER_KERNEL_BOOT_NAME}" >/dev/null 2>&1 && sync
-    if [[ "$?" -eq "0" && -s "${KERNEL_DOWNLOAD_PATH}/${SERVER_KERNEL_BOOT_NAME}" ]]; then
+    boot_file_name="${server_kernel_boot##*/}"
+    server_kernel_boot_name="${boot_file_name//%2B/+}"
+    wget -c "${server_kernel_boot}" -O "${KERNEL_DOWNLOAD_PATH}/${server_kernel_boot_name}" >/dev/null 2>&1 && sync
+    if [[ "$?" -eq "0" && -s "${KERNEL_DOWNLOAD_PATH}/${server_kernel_boot_name}" ]]; then
         tolog "03.03 The boot file complete."
     else
         tolog "03.04 The boot file failed to download." "1"
     fi
     sleep 3
 
-    # Download dtb file from the kernel directory under the path: ${SERVER_KERNEL_URL}/${DOWNLOAD_VERSION}/
-    SERVER_KERNEL_DTB="$(curl -s "${SERVER_KERNEL_URL}/${DOWNLOAD_VERSION}" | grep "download_url" | grep -o "https.*/dtb-${MYDTB_FILE}-${DOWNLOAD_VERSION}.*.tar.gz" | head -n 1)"
-    # Download dtb file from current path: ${SERVER_KERNEL_URL}/
-    if [ -z "${SERVER_KERNEL_DTB}" ]; then
-        SERVER_KERNEL_DTB="$(curl -s "${SERVER_KERNEL_URL}" | grep "download_url" | grep -o "https.*/dtb-${MYDTB_FILE}-${DOWNLOAD_VERSION}.*.tar.gz" | head -n 1)"
+    # Download dtb file from the kernel directory under the path: ${server_kernel_url}/${download_version}/
+    server_kernel_dtb="$(curl -s "${server_kernel_url}/${download_version}" | grep "download_url" | grep -o "https.*/dtb-${MYDTB_FILE}-${download_version}.*.tar.gz" | head -n 1)"
+    # Download dtb file from current path: ${server_kernel_url}/
+    if [ -z "${server_kernel_dtb}" ]; then
+        server_kernel_dtb="$(curl -s "${server_kernel_url}" | grep "download_url" | grep -o "https.*/dtb-${MYDTB_FILE}-${download_version}.*.tar.gz" | head -n 1)"
     fi
-    SERVER_KERNEL_DTB_NAME="${SERVER_KERNEL_DTB##*/}"
-    SERVER_KERNEL_DTB_NAME="${SERVER_KERNEL_DTB_NAME//%2B/+}"
-    wget -c "${SERVER_KERNEL_DTB}" -O "${KERNEL_DOWNLOAD_PATH}/${SERVER_KERNEL_DTB_NAME}" >/dev/null 2>&1 && sync
-    if [[ "$?" -eq "0" && -s "${KERNEL_DOWNLOAD_PATH}/${SERVER_KERNEL_DTB_NAME}" ]]; then
+    dtb_file_name="${server_kernel_dtb##*/}"
+    server_kernel_dtb_name="${dtb_file_name//%2B/+}"
+    wget -c "${server_kernel_dtb}" -O "${KERNEL_DOWNLOAD_PATH}/${server_kernel_dtb_name}" >/dev/null 2>&1 && sync
+    if [[ "$?" -eq "0" && -s "${KERNEL_DOWNLOAD_PATH}/${server_kernel_dtb_name}" ]]; then
         tolog "03.05 The dtb file complete."
     else
         tolog "03.06 The dtb file failed to download." "1"
     fi
     sleep 3
 
-    # Download modules file from the kernel directory under the path: ${SERVER_KERNEL_URL}/${DOWNLOAD_VERSION}/
-    SERVER_KERNEL_MODULES="$(curl -s "${SERVER_KERNEL_URL}/${DOWNLOAD_VERSION}" | grep "download_url" | grep -o "https.*/modules-${DOWNLOAD_VERSION}.*.tar.gz" | head -n 1)"
-    # Download modules file from current path: ${SERVER_KERNEL_URL}/
-    if [ -z "${SERVER_KERNEL_MODULES}" ]; then
-        SERVER_KERNEL_MODULES="$(curl -s "${SERVER_KERNEL_URL}" | grep "download_url" | grep -o "https.*/modules-${DOWNLOAD_VERSION}.*.tar.gz" | head -n 1)"
+    # Download modules file from the kernel directory under the path: ${server_kernel_url}/${download_version}/
+    server_kernel_modules="$(curl -s "${server_kernel_url}/${download_version}" | grep "download_url" | grep -o "https.*/modules-${download_version}.*.tar.gz" | head -n 1)"
+    # Download modules file from current path: ${server_kernel_url}/
+    if [ -z "${server_kernel_modules}" ]; then
+        server_kernel_modules="$(curl -s "${server_kernel_url}" | grep "download_url" | grep -o "https.*/modules-${download_version}.*.tar.gz" | head -n 1)"
     fi
-    SERVER_KERNEL_MODULES_NAME="${SERVER_KERNEL_MODULES##*/}"
-    SERVER_KERNEL_MODULES_NAME="${SERVER_KERNEL_MODULES_NAME//%2B/+}"
-    wget -c "${SERVER_KERNEL_MODULES}" -O "${KERNEL_DOWNLOAD_PATH}/${SERVER_KERNEL_MODULES_NAME}" >/dev/null 2>&1 && sync
-    if [[ "$?" -eq "0" && -s "${KERNEL_DOWNLOAD_PATH}/${SERVER_KERNEL_MODULES_NAME}" ]]; then
+    modules_file_name="${server_kernel_modules##*/}"
+    server_kernel_modules_name="${modules_file_name//%2B/+}"
+    wget -c "${server_kernel_modules}" -O "${KERNEL_DOWNLOAD_PATH}/${server_kernel_modules_name}" >/dev/null 2>&1 && sync
+    if [[ "$?" -eq "0" && -s "${KERNEL_DOWNLOAD_PATH}/${server_kernel_modules_name}" ]]; then
         tolog "03.07 The modules file complete."
     else
         tolog "03.08 The modules file failed to download." "1"
