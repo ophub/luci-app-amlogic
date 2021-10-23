b39b9c8ca38c5f98caeaed2bd3ff210aea14b39b
ophub <ophub2020@gmail.com>
2021-10-24 07:12:02 +0800

    Optimize github api query logic

--- luci-app-amlogic/root/usr/share/amlogic/amlogic_check_firmware.sh
+++ luci-app-amlogic/root/usr/share/amlogic/amlogic_check_firmware.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Set a fixed value
+check_option=${1}
+download_version=${2}
 TMP_CHECK_DIR="/tmp/amlogic"
 AMLOGIC_SOC_FILE="/etc/flippy-openwrt-release"
 START_LOG="${TMP_CHECK_DIR}/amlogic_check_firmware.log"
 LOG_FILE="${TMP_CHECK_DIR}/amlogic.log"
+github_api_openwrt="${TMP_CHECK_DIR}/github_api_openwrt"
 LOGTIME=$(date "+%Y-%m-%d %H:%M:%S")
 [[ -d ${TMP_CHECK_DIR} ]] || mkdir -p ${TMP_CHECK_DIR}
 
@@ -62,14 +65,14 @@ else
 fi
 [[ ! -z "${SOC}" ]] || tolog "The custom firmware soc is invalid." "1"
 tolog "Device: ${MYDEVICE_NAME} [ ${SOC} ], Use in [ ${EMMC_NAME} ]"
-sleep 3
+sleep 2
 
 # 01. Query local version information
 tolog "01. Query version information."
 # 01.01 Query the current version
 current_kernel_v=$(ls /lib/modules/  2>/dev/null | grep -oE '^[1-9].[0-9]{1,3}.[0-9]+')
 tolog "01.01 current version: ${current_kernel_v}"
-sleep 3
+sleep 2
 
 # 01.01 Version comparison
 main_line_ver=$(echo "${current_kernel_v}" | cut -d '.' -f1)
@@ -81,7 +84,7 @@ server_kernel_branch=$(uci get amlogic.config.amlogic_kernel_branch 2>/dev/null 
 if [[ -n "${server_kernel_branch}" && "${server_kernel_branch}" != "${main_line_version}" ]]; then
     main_line_version="${server_kernel_branch}"
     tolog "01.02 Select branch: ${main_line_version}"
-    sleep 3
+    sleep 2
 fi
 
 # 01.03. Download server version documentation
@@ -108,13 +111,30 @@ firmware_download_url="https:.*${releases_tag_keywords}.*${SOC}.*${main_line_ver
 # 02. Check Updated
 check_updated() {
     tolog "02. Start checking the updated ..."
+    curl -s "https://api.github.com/repos/${server_firmware_url}/releases" > ${github_api_openwrt} && sync
+    sleep 1
 
     # Get the openwrt firmware updated_at
-    firmware_browser_download_line=$(curl -s "https://api.github.com/repos/${server_firmware_url}/releases" | grep -n "${firmware_download_url}" | awk -F ":" '{print $1}' | head -n 1)
-    if [[ -n "${firmware_browser_download_line}" && "${firmware_browser_download_line}" -gt "0" ]]; then
-        firmware_updated_line=$(( firmware_browser_download_line - 1 ))
-        firmware_releases_updated=$(curl -s "https://api.github.com/repos/${server_firmware_url}/releases" | sed -n "${firmware_updated_line}p" | cut -d '"' -f4 | cut -d 'T' -f1)
-        tolog '<input type="button" class="cbi-button cbi-button-reload" value="Download" onclick="return b_check_firmware(this, '"'download'"')"/> Latest updated: '${firmware_releases_updated}''
+    api_down_line_array=$( cat ${github_api_openwrt} | grep -n "${firmware_download_url}" | awk -F ":" '{print $1}' | tr "\n" " " | echo $(xargs) )
+    # return: 123 233 312
+
+    i=1
+    api_updated_at=()
+    api_updated_merge=()
+    for x in ${api_down_line_array}; do
+      api_updated_at[${i}]="$( cat ${github_api_openwrt} | sed -n "$((x-1))p" | cut -d '"' -f4)"
+      api_updated_merge[${i}]="${x}@$( cat ${github_api_openwrt} | sed -n "$((x-1))p" | cut -d '"' -f4)"
+      let i++
+    done
+    # return: api_updated_at: 2021-10-21T17:52:56Z 2021-10-21T11:22:39Z 2021-10-22T17:52:56Z
+    latest_updated_at=$( echo ${api_updated_at[*]} | tr ' ' '\n' | sort -r | head -n 1 )
+    latest_updated_at_format=$( echo ${latest_updated_at} | tr 'T' '(' | tr 'Z' ')' )
+    # return: latest_updated_at: 2021-10-22T17:52:56Z
+    api_op_down_line=$( echo ${api_updated_merge[*]} | tr ' ' '\n' | grep ${latest_updated_at} | cut -d '@' -f1)
+    # return: api_openwrt_download_line: 123
+
+    if [[ -n "${api_op_down_line}" && -n "$(echo ${api_op_down_line} | sed -n "/^[0-9]\+$/p")" ]]; then
+        tolog '<input type="button" class="cbi-button cbi-button-reload" value="Download" onclick="return b_check_firmware(this, '"'download_${api_op_down_line}'"')"/> Latest updated: '${latest_updated_at_format}''
     else
         tolog "02.02 Invalid firmware check." "1"
     fi
@@ -125,9 +145,17 @@ check_updated() {
 # 03. Download Openwrt firmware
 download_firmware() {
     tolog "03. Download Openwrt firmware ..."
+
     # Get the openwrt firmware download path
-    firmware_releases_path=$(curl -s "https://api.github.com/repos/${server_firmware_url}/releases" | grep "browser_download_url" | grep -o "${firmware_download_url}" | head -n 1)
-    firmware_download_name="openwrt_${SOC}_k${main_line_version}_update${firmware_suffix}"
+    if [[ ${download_version} == download* ]]; then
+        download_version=$(echo "${download_version}" | cut -d '_' -f2)
+        tolog "03.01 Start downloading..."
+    else
+        tolog "03.02 Invalid parameter" "1"
+    fi
+
+    firmware_releases_path=$( cat ${github_api_openwrt} | sed -n "${download_version}p" | grep "browser_download_url" | grep -o "${firmware_download_url}" | head -n 1)
+    firmware_download_name="openwrt_${SOC}_k${main_line_version}_github${firmware_suffix}"
     wget -c "${firmware_releases_path}" -O "${FIRMWARE_DOWNLOAD_PATH}/${firmware_download_name}" >/dev/null 2>&1 && sync
     if [[ "$?" -eq "0" && -s "${FIRMWARE_DOWNLOAD_PATH}/${firmware_download_name}" ]]; then
         tolog "03.01 OpenWrt firmware download complete, you can update."
@@ -135,6 +163,9 @@ download_firmware() {
         tolog "03.02 Invalid firmware download." "1"
     fi
     sleep 3
+
+    # Delete temporary files
+    rm -f ${github_api_openwrt} 2>/dev/null && sync
 
     #echo '<a href="javascript:;" onclick="return amlogic_update(this, '"'${firmware_download_name}'"')">Update</a>' >$START_LOG
     tolog '<input type="button" class="cbi-button cbi-button-reload" value="Update" onclick="return amlogic_update(this, '"'${firmware_download_name}'"')"/>'
