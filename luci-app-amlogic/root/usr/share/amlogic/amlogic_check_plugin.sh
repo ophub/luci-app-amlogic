bef51f2d5b3b9f8da8b22938d1a3a66101a2c5d8
ophub <ophub2020@gmail.com>
2021-10-28 16:05:04 +0800

    Adjust plugin download query

--- luci-app-amlogic/root/usr/share/amlogic/amlogic_check_plugin.sh
+++ luci-app-amlogic/root/usr/share/amlogic/amlogic_check_plugin.sh
@@ -5,6 +5,7 @@ TMP_CHECK_DIR="/tmp/amlogic"
 AMLOGIC_SOC_FILE="/etc/flippy-openwrt-release"
 START_LOG="${TMP_CHECK_DIR}/amlogic_check_plugin.log"
 LOG_FILE="${TMP_CHECK_DIR}/amlogic.log"
+github_api_plugin="${TMP_CHECK_DIR}/github_api_plugin"
 LOGTIME=$(date "+%Y-%m-%d %H:%M:%S")
 [[ -d ${TMP_CHECK_DIR} ]] || mkdir -p ${TMP_CHECK_DIR}
 rm -f ${TMP_CHECK_DIR}/*.ipk 2>/dev/null && sync
@@ -72,53 +73,66 @@ sleep 3
 
 # 02. Check the version on the server
 tolog "02. Query server version information."
-server_plugin_version=$(curl -i -s "https://api.github.com/repos/ophub/luci-app-amlogic/releases" | grep "tag_name" | head -n 1 | awk -F '"' '{print $4}')
+
+curl -s "https://api.github.com/repos/ophub/luci-app-amlogic/releases" > ${github_api_plugin} && sync
+sleep 1
+
+server_plugin_version=$( cat ${github_api_plugin} | grep "tag_name"  | awk -F '"' '{print $4}' | sort -r | head -n 1 )
 [ -n "${server_plugin_version}" ] || tolog "02.01 Failed to get the version on the server." "1"
-tolog "02.02 current version: ${current_plugin_v}, Latest version: ${server_plugin_version}"
+tolog "02.01 current version: ${current_plugin_v}, Latest version: ${server_plugin_version}"
 sleep 3
 
 if [[ "${current_plugin_v}" == "${server_plugin_version}" ]]; then
-    tolog "02.03 Already the latest version, no need to update." "1"
+    tolog "02.02 Already the latest version, no need to update." "1"
     sleep 5
     tolog ""
 else
-    tolog "02.04 Automatically download the latest plugin."
+    tolog "02.03 Check the latest plug-in download address."
 
     server_plugin_url="https://github.com/ophub/luci-app-amlogic/releases/download"
-    server_plugin_file_ipk="luci-app-amlogic_${server_plugin_version}_all.ipk"
-    server_plugin_file_i18n="luci-i18n-amlogic-zh-cn_${server_plugin_version}_all.ipk"
-    server_plugin_file_libfs="luci-lib-fs_1.0-1_all.ipk"
+    server_plugin_file_ipk="$( cat ${github_api_plugin} | grep -E "browser_.*${server_plugin_version}.*" | grep -oE  "luci-app-amlogic_.*.ipk" | head -n 1 )"
+    server_plugin_file_i18n="$( cat ${github_api_plugin} | grep -E "browser_.*${server_plugin_version}.*" | grep -oE  "luci-i18n-amlogic-zh-cn_.*.ipk" | head -n 1 )"
+    server_plugin_file_libfs="$( cat ${github_api_plugin} | grep -E "browser_.*${server_plugin_version}.*" | grep -oE  "luci-lib-fs_.*.ipk" | head -n 1 )"
+
+    if [[ -n "${server_plugin_file_ipk}" && -n "${server_plugin_file_i18n}" && -n "${server_plugin_file_libfs}" ]]; then
+        tolog "02.04 Start downloading the latest plugin..."
+    else
+        tolog "02.04 No available plugins found!" "1"
+    fi
 
     # Download plugin ipk file
     wget -c "${server_plugin_url}/${server_plugin_version}/${server_plugin_file_ipk}" -O "${TMP_CHECK_DIR}/${server_plugin_file_ipk}" >/dev/null 2>&1 && sync
     if [[ "$?" -eq "0" && -s "${TMP_CHECK_DIR}/${server_plugin_file_ipk}" ]]; then
         tolog "02.05 ${server_plugin_file_ipk} complete."
     else
-        tolog "02.06 The plugin file failed to download." "1"
+        tolog "02.05 The plugin file failed to download." "1"
     fi
     sleep 3
 
     # Download plugin i18n file
     wget -c "${server_plugin_url}/${server_plugin_version}/${server_plugin_file_i18n}" -O "${TMP_CHECK_DIR}/${server_plugin_file_i18n}" >/dev/null 2>&1 && sync
     if [[ "$?" -eq "0" && -s "${TMP_CHECK_DIR}/${server_plugin_file_i18n}" ]]; then
-        tolog "02.07 ${server_plugin_file_i18n} complete."
+        tolog "02.06 ${server_plugin_file_i18n} complete."
     else
-        tolog "02.08 The plugin i18n failed to download." "1"
+        tolog "02.06 The plugin i18n failed to download." "1"
     fi
     sleep 3
 
     # Download plugin lib-fs file
     wget -c "${server_plugin_url}/${server_plugin_version}/${server_plugin_file_libfs}" -O "${TMP_CHECK_DIR}/${server_plugin_file_libfs}" >/dev/null 2>&1 && sync
     if [[ "$?" -eq "0" && -s "${TMP_CHECK_DIR}/${server_plugin_file_libfs}" ]]; then
-        tolog "02.09 ${server_plugin_file_libfs} complete."
+        tolog "02.07 ${server_plugin_file_libfs} complete."
     else
-        tolog "02.10 The plugin i18n failed to download." "1"
+        tolog "02.07 The plugin i18n failed to download." "1"
     fi
     sleep 3
 fi
 
 tolog "03. The plug is ready, you can update."
 sleep 3
+
+# Delete temporary files
+rm -f ${github_api_plugin} 2>/dev/null && sync
 
 #echo '<a href=upload>Update</a>' >$START_LOG
 tolog '<input type="button" class="cbi-button cbi-button-reload" value="Update" onclick="return amlogic_plugin(this)"/>'
