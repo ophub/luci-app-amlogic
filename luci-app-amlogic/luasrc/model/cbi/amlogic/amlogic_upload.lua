c7a3e7240a14089deb544f647d572b7ec01b78d6
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2021-11-04 17:07:29 +0000

    luci-app-amlogic: tidy up code

--- luci-app-amlogic/luasrc/model/cbi/amlogic/amlogic_upload.lua
+++ luci-app-amlogic/luasrc/model/cbi/amlogic/amlogic_upload.lua
@@ -5,24 +5,24 @@ local b, form
 
 --Remove the spaces in the string
 function trim(str)
-   --return (string.gsub(str, "^%s*(.-)%s*$", "%1"))
-   return (string.gsub(str, "%s+", ""))
+	--return (string.gsub(str, "^%s*(.-)%s*$", "%1"))
+	return (string.gsub(str, "%s+", ""))
 end
 
 --Set default upload path
 local ROOT_PTNAME = trim(luci.sys.exec("df / | tail -n1 | awk '{print $1}' | awk -F '/' '{print $3}'"))
 if ROOT_PTNAME then
-    if (string.find(ROOT_PTNAME, "mmcblk[0-4]p[1-4]")) then
-        local EMMC_NAME = trim(luci.sys.exec("echo " .. ROOT_PTNAME .. " | awk '{print substr($1, 1, length($1)-2)}'"))
-        upload_path = trim("/mnt/" .. EMMC_NAME .. "p4/")
-    elseif (string.find(ROOT_PTNAME, "[hsv]d[a-z]")) then
-        local EMMC_NAME = trim(luci.sys.exec("echo " .. ROOT_PTNAME .. " | awk '{print substr($1, 1, length($1)-1)}'"))
-        upload_path = trim("/mnt/" .. EMMC_NAME .. "4/")
-    else
-        upload_path = "/tmp/upload/"
-    end
+	if (string.find(ROOT_PTNAME, "mmcblk[0-4]p[1-4]")) then
+		local EMMC_NAME = trim(luci.sys.exec("echo " .. ROOT_PTNAME .. " | awk '{print substr($1, 1, length($1)-2)}'"))
+		upload_path = trim("/mnt/" .. EMMC_NAME .. "p4/")
+	elseif (string.find(ROOT_PTNAME, "[hsv]d[a-z]")) then
+		local EMMC_NAME = trim(luci.sys.exec("echo " .. ROOT_PTNAME .. " | awk '{print substr($1, 1, length($1)-1)}'"))
+		upload_path = trim("/mnt/" .. EMMC_NAME .. "4/")
+	else
+		upload_path = "/tmp/upload/"
+	end
 else
-    upload_path = "/tmp/upload/"
+	upload_path = "/tmp/upload/"
 end
 
 --Clear the version check log
@@ -30,13 +30,17 @@ luci.sys.exec("echo '' > /tmp/amlogic/amlogic_check_plugin.log && sync >/dev/nul
 luci.sys.exec("echo '' > /tmp/amlogic/amlogic_check_kernel.log && sync >/dev/null 2>&1")
 
 --SimpleForm for Update OpenWrt firmware/kernel
-b = SimpleForm("upload", translate("Upload"), nil)
+b = SimpleForm("upload", nil)
+b.title = translate("Upload")
 b.description = translate("After uploading [Firmware], [Kernel], [IPK] or [Backup Config], the operation buttons will be displayed.")
 b.reset = false
 b.submit = false
+
 s = b:section(SimpleSection, "", "")
+
 o = s:option(FileUpload, "")
 o.template = "amlogic/other_upload"
+
 um = s:option(DummyValue, "", nil)
 um.template = "amlogic/other_dvalue"
 
@@ -78,60 +82,60 @@ local function getSizeStr(size)
 		size = size / 1024
 		i = i + 1
 	until(size <= 1024)
-    return string.format("%.1f", size) .. byteUnits[i]
+	return string.format("%.1f", size) .. byteUnits[i]
 end
 
 local inits, attr = {}
 for i, f in ipairs(fs.glob(trim(upload_path .. "*"))) do
-    attr = fs.stat(f)
-    itisfile = fs.isfile(f)
-    if attr and itisfile then
-        inits[i] = {}
-        inits[i].name = fs.basename(f)
-        inits[i].mtime = os.date("%Y-%m-%d %H:%M:%S", attr.mtime)
-        inits[i].modestr = attr.modestr
-        inits[i].size = getSizeStr(attr.size)
-        inits[i].remove = 0
-        inits[i].ipk = false
+	attr = fs.stat(f)
+	itisfile = fs.isfile(f)
+	if attr and itisfile then
+		inits[i] = {}
+		inits[i].name = fs.basename(f)
+		inits[i].mtime = os.date("%Y-%m-%d %H:%M:%S", attr.mtime)
+		inits[i].modestr = attr.modestr
+		inits[i].size = getSizeStr(attr.size)
+		inits[i].remove = 0
+		inits[i].ipk = false
 
-        --Check whether the openwrt firmware file
-        -- openwrt_s905d_v5.10.16_2021.05.31.1958.img.gz
-        if (string.lower(string.sub(fs.basename(f), -7, -1)) == ".img.gz") then
-            openwrt_firmware_file = true
-        end
-        -- openwrt_s905d_n1_R21.7.15_k5.4.134-flippy-62+o.img.xz
-        if (string.lower(string.sub(fs.basename(f), -7, -1)) == ".img.xz") then
-            openwrt_firmware_file = true
-        end
-        -- openwrt_s905d_n1_R21.7.15_k5.13.2-flippy-62+.7z
-        if (string.lower(string.sub(fs.basename(f), -3, -1)) == ".7z") then
-            openwrt_firmware_file = true
-        end
-        -- openwrt_s905d_n1_R21.7.15_k5.13.2-flippy-62+.img
-        if (string.lower(string.sub(fs.basename(f), -4, -1)) == ".img") then
-            openwrt_firmware_file = true
-        end
+		--Check whether the openwrt firmware file
+		-- openwrt_s905d_v5.10.16_2021.05.31.1958.img.gz
+		if (string.lower(string.sub(fs.basename(f), -7, -1)) == ".img.gz") then
+			openwrt_firmware_file = true
+		end
+		-- openwrt_s905d_n1_R21.7.15_k5.4.134-flippy-62+o.img.xz
+		if (string.lower(string.sub(fs.basename(f), -7, -1)) == ".img.xz") then
+			openwrt_firmware_file = true
+		end
+		-- openwrt_s905d_n1_R21.7.15_k5.13.2-flippy-62+.7z
+		if (string.lower(string.sub(fs.basename(f), -3, -1)) == ".7z") then
+			openwrt_firmware_file = true
+		end
+		-- openwrt_s905d_n1_R21.7.15_k5.13.2-flippy-62+.img
+		if (string.lower(string.sub(fs.basename(f), -4, -1)) == ".img") then
+			openwrt_firmware_file = true
+		end
 
-        --Check whether the three kernel files
-        -- boot-5.10.16-flippy-53+.tar.gz
-        if (string.lower(string.sub(fs.basename(f), 1, 5)) == "boot-") then
-            boot_file = true
-        end
-        -- dtb-amlogic-5.10.16-flippy-53+.tar.gz
-        if (string.lower(string.sub(fs.basename(f), 1, 4)) == "dtb-") then
-            dtb_file = true
-        end
-        -- modules-5.10.16-flippy-53+.tar.gz
-        if (string.lower(string.sub(fs.basename(f), 1, 8)) == "modules-") then
-            modules_file = true
-        end
+		--Check whether the three kernel files
+		-- boot-5.10.16-flippy-53+.tar.gz
+		if (string.lower(string.sub(fs.basename(f), 1, 5)) == "boot-") then
+			boot_file = true
+		end
+		-- dtb-amlogic-5.10.16-flippy-53+.tar.gz
+		if (string.lower(string.sub(fs.basename(f), 1, 4)) == "dtb-") then
+			dtb_file = true
+		end
+		-- modules-5.10.16-flippy-53+.tar.gz
+		if (string.lower(string.sub(fs.basename(f), 1, 8)) == "modules-") then
+			modules_file = true
+		end
 
-        --Check whether the backup file
-        -- openwrt_config.tar.gz
-        if (string.lower(string.sub(fs.basename(f), 1, -1)) == "openwrt_config.tar.gz") then
-            backup_config_file = true
-        end
-    end
+		--Check whether the backup file
+		-- openwrt_config.tar.gz
+		if (string.lower(string.sub(fs.basename(f), 1, -1)) == "openwrt_config.tar.gz") then
+			backup_config_file = true
+		end
+	end
 end
 
 --SimpleForm for Upload file list
@@ -143,21 +147,21 @@ description_info = ""
 luci.sys.exec("echo '' > /tmp/amlogic/amlogic_check_upfiles.log && sync >/dev/null 2>&1")
 
 if backup_config_file then
-    description_info = description_info .. translate("There are config file in the upload directory, and you can restore the config. ")
+	description_info = description_info .. translate("There are config file in the upload directory, and you can restore the config. ")
 end
 
 if boot_file and dtb_file and modules_file then
-    description_info = description_info .. translate("There are kernel files in the upload directory, and you can replace the kernel.")
-    luci.sys.exec("echo 'kernel' > /tmp/amlogic/amlogic_check_upfiles.log && sync >/dev/null 2>&1")
+	description_info = description_info .. translate("There are kernel files in the upload directory, and you can replace the kernel.")
+	luci.sys.exec("echo 'kernel' > /tmp/amlogic/amlogic_check_upfiles.log && sync >/dev/null 2>&1")
 end
 
 if openwrt_firmware_file then
-    description_info = description_info .. translate("There are openwrt firmware file in the upload directory, and you can update the openwrt.")
-    luci.sys.exec("echo 'firmware' > /tmp/amlogic/amlogic_check_upfiles.log && sync >/dev/null 2>&1")
+	description_info = description_info .. translate("There are openwrt firmware file in the upload directory, and you can update the openwrt.")
+	luci.sys.exec("echo 'firmware' > /tmp/amlogic/amlogic_check_upfiles.log && sync >/dev/null 2>&1")
 end
 
 if description_info ~= "" then
-    form.description =  ' <span style="color: green"><b> Tip: ' .. description_info .. ' </b></span> '
+	form.description =  ' <span style="color: green"><b> Tip: ' .. description_info .. ' </b></span> '
 end
 
 tb = form:section(Table, inits)
@@ -207,19 +211,18 @@ btnis.render = function(self, section, scope)
 	Button.render(self, section, scope)
 end
 btnis.write = function(self, section)
-    if IsIpkFile(inits[section].name) then
-        local r = luci.sys.exec(string.format('opkg --force-reinstall install ' .. upload_path .. '%s', inits[section].name))
+	if IsIpkFile(inits[section].name) then
+		local r = luci.sys.exec(string.format('opkg --force-reinstall install ' .. upload_path .. '%s', inits[section].name))
 	local x = luci.sys.exec("rm -rf /tmp/luci-indexcache /tmp/luci-modulecache/* 2>/dev/null")
-        form.description = string.format('<span style="color: red">%s</span>', r)
-    elseif IsConfigFile(inits[section].name) then
-        form.description =  ' <span style="color: green"><b> ' .. translate("Tip: The config is being restored, and it will automatically restart after completion.") .. ' </b></span> '
-        local x = luci.sys.exec("chmod +x /usr/sbin/openwrt-backup 2>/dev/null")
-        local r = luci.sys.exec("/usr/sbin/openwrt-backup -r > /tmp/amlogic/amlogic.log && sync 2>/dev/null")
-    end
+		form.description = string.format('<span style="color: red">%s</span>', r)
+	elseif IsConfigFile(inits[section].name) then
+		form.description =  ' <span style="color: green"><b> ' .. translate("Tip: The config is being restored, and it will automatically restart after completion.") .. ' </b></span> '
+		local x = luci.sys.exec("chmod +x /usr/sbin/openwrt-backup 2>/dev/null")
+		local r = luci.sys.exec("/usr/sbin/openwrt-backup -r > /tmp/amlogic/amlogic.log && sync 2>/dev/null")
+	end
 end
 
 --SimpleForm for Check upload files
 form:section(SimpleSection).template  = "amlogic/other_upfiles"
 
 return b, form
-
