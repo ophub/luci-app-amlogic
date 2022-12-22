9d13489bc547096891b7c598b3f9f794367e6d54
ophub <ophub2020@gmail.com>
2022-12-22 12:45:24 +0000

    Optimize code logic for onekey-install script

--- onekey-install.sh
+++ onekey-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#================================================================================
+#========================================================================================
+#
 # This file is licensed under the terms of the GNU General Public
 # License version 2. This program is licensed "as is" without any
 # warranty of any kind, whether express or implied.
@@ -7,72 +8,97 @@
 # This file is a part of the luci-app-amlogic plugin
 # https://github.com/ophub/luci-app-amlogic
 #
-# Description: Install luci-app-amlogic plugin for OpenWrt on amlogic s9xxx boxs
+# Description: Install luci-app-amlogic plugin for OpenWrt
 # Copyright (C) 2021- https://github.com/unifreq/openwrt_packit
 # Copyright (C) 2021- https://github.com/ophub/luci-app-amlogic
 #
 # Command: curl -fsSL git.io/luci-app-amlogic | bash
-#================================================================================
-
+#
+#==================================== Functions list ====================================
+#
+# process_msg      : Output process message
+# query_version    : Query the latest version
+# download_plugin  : Download plug-in
+# install_plugin   : Install plug-in
+#
+#============================ Set make environment variables ============================
+#
 # Set a fixed value
-TMP_CHECK_DIR="/root"
-github_api_plugin="${TMP_CHECK_DIR}/github_api_plugin"
-rm -f ${TMP_CHECK_DIR}/*.ipk && sync
+tmp_dir="/root"
+github_api_file="${tmp_dir}/github_api_file"
+#
+#========================================================================================
 
-# Log function
-tolog() {
+process_msg() {
     echo -e "${1}"
     [[ -z "${2}" ]] || exit 1
 }
 
-# 01. Check the version on the server
-tolog "01. Query server version information."
+query_version() {
+    process_msg "01. Query server version information."
 
-curl -s "https://api.github.com/repos/ophub/luci-app-amlogic/releases" >${github_api_plugin} && sync
-sleep 1
+    # Delete other ipk files
+    rm -f ${github_api_file}
 
-server_plugin_version=$(cat ${github_api_plugin} | grep "tag_name" | awk -F '"' '{print $4}' | tr " " "\n" | sort -rV | head -n 1)
-[ -n "${server_plugin_version}" ] || tolog "01.01 Failed to get the version on the server." "1"
-tolog "01.01 Latest version: ${server_plugin_version}"
-sleep 3
+    # Call API interface to query plug-in information
+    curl -s "https://api.github.com/repos/ophub/luci-app-amlogic/releases" >${github_api_file}
+    sleep 1
 
-tolog "02. Check the latest plug-in download address."
+    # Query the latest version
+    plugin_version="$(cat ${github_api_file} | grep "tag_name" | awk -F '"' '{print $4}' | tr " " "\n" | sort -rV | head -n 1)"
+    [[ -n "${plugin_version}" ]] || process_msg "01.01 Failed to get the version on the server." "1"
+    process_msg "01.01 Latest version: ${plugin_version}"
+}
 
-server_plugin_url="https://github.com/ophub/luci-app-amlogic/releases/download"
-server_plugin_file_ipk="$(cat ${github_api_plugin} | grep -E "browser_.*${server_plugin_version}.*" | grep -oE "luci-app-amlogic_.*.ipk" | head -n 1)"
-server_plugin_file_i18n="$(cat ${github_api_plugin} | grep -E "browser_.*${server_plugin_version}.*" | grep -oE "luci-i18n-amlogic-zh-cn_.*.ipk" | head -n 1)"
+download_plugin() {
+    process_msg "02. Check the latest plug-in download address."
 
-if [[ -n "${server_plugin_file_ipk}" && -n "${server_plugin_file_i18n}" ]]; then
-    tolog "02.01 Start downloading the latest plugin..."
-else
-    tolog "02.01 No available plugins found!" "1"
-fi
+    # Delete other ipk files
+    rm -f ${tmp_dir}/*.ipk
 
-# Download plugin ipk file
-wget "${server_plugin_url}/${server_plugin_version}/${server_plugin_file_ipk}" -O "${TMP_CHECK_DIR}/${server_plugin_file_ipk}" >/dev/null 2>&1 && sync
-if [[ "$?" -eq "0" && -s "${TMP_CHECK_DIR}/${server_plugin_file_ipk}" ]]; then
-    tolog "02.02 ${server_plugin_file_ipk} complete."
-else
-    tolog "02.02 The plugin file failed to download." "1"
-fi
-sleep 3
+    # Get the plug-in download address
+    plugin_url="https://github.com/ophub/luci-app-amlogic/releases/download"
+    main_file="$(cat ${github_api_file} | grep -E "browser_.*${plugin_version}.*" | grep -oE "luci-app-amlogic_.*.ipk" | head -n 1)"
+    language_file="$(cat ${github_api_file} | grep -E "browser_.*${plugin_version}.*" | grep -oE "luci-i18n-amlogic-zh-cn_.*.ipk" | head -n 1)"
+    if [[ -n "${main_file}" && -n "${language_file}" ]]; then
+        process_msg "02.01 Start downloading the latest plugin..."
+    else
+        process_msg "02.01 No available plug-in found!" "1"
+    fi
 
-# Download plugin i18n file
-wget "${server_plugin_url}/${server_plugin_version}/${server_plugin_file_i18n}" -O "${TMP_CHECK_DIR}/${server_plugin_file_i18n}" >/dev/null 2>&1 && sync
-if [[ "$?" -eq "0" && -s "${TMP_CHECK_DIR}/${server_plugin_file_i18n}" ]]; then
-    tolog "02.03 ${server_plugin_file_i18n} complete."
-else
-    tolog "02.03 The plugin i18n failed to download." "1"
-fi
-sleep 3
+    # Download the plug-in's ipk file
+    wget "${plugin_url}/${plugin_version}/${main_file}" -O "${tmp_dir}/${main_file}"
+    if [[ "${?}" -eq "0" && -s "${tmp_dir}/${main_file}" ]]; then
+        process_msg "02.02 ${main_file} complete."
+    else
+        process_msg "02.02 The plugin file failed to download." "1"
+    fi
 
-# Automatic install
-tolog "03. Start automatic installation."
-opkg --force-reinstall install ${TMP_CHECK_DIR}/*.ipk >/dev/null 2>&1
-rm -rf /tmp/luci-indexcache /tmp/luci-modulecache/* >/dev/null 2>&1
-rm -f ${TMP_CHECK_DIR}/*.ipk 2>/dev/null && sync
-rm -f ${github_api_plugin} 2>/dev/null && sync
-tolog "03.01 The plugin has been installed successfully."
-tolog "03.02 Please login to OpenWrt -> system -> Amlogic Service."
+    # Download the plug-in's i18n file
+    wget "${plugin_url}/${plugin_version}/${language_file}" -O "${tmp_dir}/${language_file}"
+    if [[ "${?}" -eq "0" && -s "${tmp_dir}/${language_file}" ]]; then
+        process_msg "02.03 ${language_file} complete."
+    else
+        process_msg "02.03 The plugin i18n failed to download." "1"
+    fi
+}
+
+install_plugin() {
+    process_msg "03. Start automatic installation."
+
+    # Force plug-in reinstallation
+    opkg --force-reinstall install ${tmp_dir}/*.ipk
+
+    # Delete cache file
+    rm -rf /tmp/luci-indexcache /tmp/luci-modulecache/* 2>/dev/null
+    rm -f ${tmp_dir}/*.ipk
+    rm -f ${github_api_file}
+
+    process_msg "03.01 The plugin has been installed successfully, Path: System -> Amlogic Service."
+}
+
+query_version
+download_plugin
+install_plugin
 
 exit 0
